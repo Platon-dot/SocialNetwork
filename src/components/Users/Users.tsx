0ce08d2fa091e57e321e633bcc5833ed4980852d
UncleStifler <ambalexey@gmail.com>
2021-12-01 01:00:04 +0100

    Corrected the markup

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -8,11 +8,8 @@ import {Button, Col, Container, Row} from "react-bootstrap";
 import Paginator from "../../paginator/paginator";
 
 const Users = () => {
-
     let dispatch = useDispatch()
-
     let users = useSelector<RootStateType, UsersType[]>((state) => state.usersReducer.items)
-
     const followHandler = (userId: number) => {
         dispatch(followAC(userId))
     }
@@ -20,69 +17,43 @@ const Users = () => {
         dispatch(unFollowAC(userId))
     }
 
-    // const activeSelected = (selectedPage: number) => {
-    //     dispatch(setUsersTC(selectedPage, pageSize))
-    //     dispatch(changeSelectedPageAC(selectedPage))
-    // }
-    // let pagesCount = Math.ceil(totalCount / pageSize)
-    // let pages = []
-    // for (let i = 1; i <= pagesCount; i++) {
-    //     pages.push(i)
-    // }
-    // const [portionNumber, setPortionNumber] = useState(1)
-    // let leftPage = (portionNumber - 1) * pageSize + 1
-    // let rightPage = portionNumber * pageSize
-
     return (
         <>
             <Paginator/>
-            {
-                users.map(userT => {
-                    return (
-                        <Container key={userT.id}>
-                            <Row>
-                                <Col
-                                    md={{span: 3, offset: 3}}>
-                                    <div>
-                            <span>
-                                <div className="m-1">
-                                    <img
-                                        src={userT.photos.small}
-                                        alt='avatar'
-                                        className={styles.userPhoto}/>
-                                </div>
-                                <div>
-                                    {!userT.followed ?
-                                        <Button
-                                            variant="outline-primary"
-                                            onClick={() => followHandler(userT.id)}>
-                                            FOLLOW
-                                        </Button>
-                                        :
-                                        <Button
-                                            variant="outline-primary"
-                                            onClick={() => unFollowHandler(userT.id)}>
-                                            UNFOLLOW
-                                        </Button>
-                                    }
-                                </div>
-                            </span>
-                                        <span>
-                                <span>
-                                    <div className="m-1">{userT.name}</div>
-                                    <div>{userT.status}</div>
-                                </span>
-                                            {/*<span>*/}
-                                            {/*    <div>{userT.}</div>*/}
-                                            {/*    <div>{userT.location.country}</div>*/}
-                                            {/*</span>*/}
-                            </span>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </Container>
-                    )
-                })
+            {users.map(userT => {
+                return (
+                    <Container className="container-sm "
+                               key={userT.id}>
+                        <Row className="justify-content-center m-5">
+                            <Col className="col col-lg-2 bg-light">
+                                <img
+                                    src={userT.photos.small}
+                                    alt="avatar"
+                                    className={styles.userPhoto}/>
+                            </Col>
+                            <Col className="col col-lg-2 bg-light">
+                                {userT.name}
+                                {userT.status}
+                            </Col>
+                            <Col className="col col-lg-2 bg-light">
+                                {!userT.followed ?
+                                    <Button
+                                        variant="outline-primary"
+                                        onClick={() => followHandler(userT.id)}>
+                                        FOLLOW
+                                    </Button>
+                                    :
+                                    <Button
+                                        variant="outline-primary"
+                                        onClick={() => unFollowHandler(userT.id)}>
+                                        UNFOLLOW
+                                    </Button>
+                                }
+                            </Col>
+                        </Row>
+                    </Container>
+                )
+            })
             }
         </>
     )
